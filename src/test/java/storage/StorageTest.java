b75a4eea8a1b9e56621a659abb82d4bd8e195f20
chenxk619 <chenxk619@gmail.com>
2024-03-24 00:54:43 +0800

    Update file path

--- src/test/java/storage/StorageTest.java
+++ src/test/java/storage/StorageTest.java
@@ -2,6 +2,7 @@ package storage;
 import org.junit.jupiter.api.Test;
 import recipeio.recipe.RecipeList;
 import recipeio.storage.Storage;
+import recipeio.Constants;
 
 import java.io.File;
 
@@ -10,11 +11,10 @@ import static org.junit.jupiter.api.Assertions.fail;
 
 
 public class StorageTest {
-    public static final String FILE_PATH = "recipe.txt";
     @Test
     public void testExist(){
-        File f = new File(FILE_PATH);
-        assert f.getPath().equals(FILE_PATH) : "File exists";
+        File f = new File(Constants.FILE_PATH);
+        assert f.getPath().equals(Constants.FILE_PATH) : "File exists";
         if (f.exists()) {
             assertTrue(true);
         } else {
