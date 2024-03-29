c4eec307090105988421362ab87f30a57fedb253
yuki-zmstr <yukihide99@gmail.com>
2024-03-30 00:02:01 +0800

    Reformat constant strings

--- src/main/java/recipeio/Constants.java
+++ src/main/java/recipeio/Constants.java
@@ -5,8 +5,6 @@ public class Constants {
     public static final int INDEX_ID = 1;
     public static final int MAX_RECIPES = 99999;
 
-    public static final String RECIPE_NAME_PREFIX = "/name ";
-    public static final String RECIPE_TIME_PREFIX = "/time ";
     public static final String RECIPE_DELIMETER = "/";
 
     public static final String EXIT_COMMAND = "exit";
@@ -15,6 +13,7 @@ public class Constants {
     public static final String LIST_COMMAND = "list";
     public static final String HELP_COMMAND = "help";
     public static final String FIND_COMMAND = "find";
+    public static final String FILTER_COMMAND = "filter";
     public static final String DETAIL_COMMAND = "detail";
 
     public static final String INVALID_TASK_FORMAT_ERROR_MESSAGE = "Sorry. I couldn't understand. Please follow the " +
@@ -23,13 +22,11 @@ public class Constants {
             "calories!";
     public static final String MEAL_CATEGORY_ERROR_MESSAGE = "Please enter a valid meal category. Here are your" +
             "options:\n BREAKFAST, LUNCH, DINNER, APPETIZER, DESSERT";
-    public static final String NO_RECIPES_ERROR_MESSAGE = "Sorry, you have no recipes to find matches with. " +
-            "Try adding some!";
+    public static final String NO_RECIPES_ERROR_MESSAGE = "\tSorry, you have no recipes. Try adding some!";
+    public static final String NO_MATCHES_ERROR_MESSAGE = "\tThere were no matches. Try searching for something else. ";
     public static final String MESSAGE_ASK_INPUT = "asking user for input";
     public static final String FIND_BY_KEYWORD = "kw";
     public static final String FIND_BY_DATE = "date";
     public static final String FIND_BY_ALLERGY = "allergy";
-    static final String REGEX_MATCHER = RECIPE_NAME_PREFIX + "|"
-            + RECIPE_TIME_PREFIX;
 
 }
