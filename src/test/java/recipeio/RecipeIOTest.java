e31a4ad37b38518fc91fa015a9e70e493550e7fd
chenxk619 <chenxk619@gmail.com>
2024-03-29 16:18:47 +0800

    Restore file saving and loading, and update RecipeIOTest

--- src/test/java/recipeio/RecipeIOTest.java
+++ src/test/java/recipeio/RecipeIOTest.java
@@ -6,61 +6,55 @@ import recipeio.recipe.Recipe;
 import recipeio.recipe.RecipeList;
 
 import java.util.ArrayList;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 class RecipeIOTest {
 
-    RecipeList recipes;
-    ArrayList<String> allergies;
-    Recipe dummyRecipe;
-
-    public RecipeIOTest() {
-        this.recipes = new RecipeList();
-        this.allergies = new ArrayList<>();
-        allergies.add("eggs");
-        allergies.add("dairy");
-        this.dummyRecipe = new Recipe(
-                "Cream Spaghetti",
-                60,
-                500,
-                allergies,
-                MealCategory.LUNCH,
-                "www.spaghetti.com"
-        );
-    }
-
     @Test
     public void testAddRecipe() {
-        recipes.add(dummyRecipe);
-        assertEquals(1, recipes.getSize());
+        RecipeList recipes = new RecipeList();
+        ArrayList<String> allergies = new ArrayList<String>();
+        int originalSize = recipes.getSize();
+        allergies.add("eggs");
+        Recipe newRecipe = new Recipe("cookies", 40, 350, allergies,
+                MealCategory.DESSERT, "THIS IS MY URL");
+        recipes.add(newRecipe);
+        assertTrue(recipes.getSize() == originalSize + 1);
     }
 
     @Test
     public void testDeleteRecipe() {
-        recipes.add(dummyRecipe);
+        RecipeList recipes = new RecipeList();
+        ArrayList<String> allergies = new ArrayList<String>();
+        int originalSize = recipes.getSize();
+        allergies.add("eggs");
+        Recipe newRecipe = new Recipe("cookies", 40, 350, allergies,
+                MealCategory.DESSERT, "THIS IS MY URL");
+        recipes.add(newRecipe);
         recipes.delete(1);
-        assertEquals(0, recipes.getSize());
-    }
-    
-    @Test
-    public void testStringConversion() {
-        assertEquals("Cream Spaghetti / 60 min / 500 kcal / url: www.spaghetti.com", dummyRecipe.toString());
+        assertTrue(recipes.getSize() == originalSize);
     }
 
+    @Test
+    public void testStringConversion() {
+        Recipe testRecipe = new Recipe("Spaghetti Carbonara", 0, 0, null,
+                MealCategory.LUNCH, null);
+        assertEquals("Spaghetti Carbonara / 0 min / 0 kcal / url: null", testRecipe.toString());
+    }
 
     @Test
     public void testFindAllergy() {
         ArrayList<String> testAllergies = new ArrayList<>();
         testAllergies.add("eggs");
-        Recipe testRecipe = new Recipe("Cream Spaghetti", 0, 0, testAllergies,
+        Recipe testRecipe = new Recipe("Spaghetti Carbonara", 0, 0, testAllergies,
                 MealCategory.LUNCH, null);
         assert testRecipe.allergies.toString().equals("[eggs]") : "correct allergy";
         RecipeList testRecipeList = new RecipeList();
         testRecipeList.add(testRecipe);
         testRecipeList.findAllergy("eggs");
-        String expectedOutput = "List of recipes with eggs mentioned:\nCream Spaghetti\n";
+        String expectedOutput = "List of recipes with eggs mentioned:\nSpaghetti Carbonara\n";
         assertEquals(testRecipeList.findAllergy("eggs"), expectedOutput);
     }
 }
