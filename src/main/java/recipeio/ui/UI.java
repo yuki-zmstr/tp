567d89721bb33ee0b2ffa554780167beb9acd781
yuki-zmstr <yukihide99@gmail.com>
2024-03-30 00:04:04 +0800

    Refactor help instructions and print Allergies method

--- src/main/java/recipeio/ui/UI.java
+++ src/main/java/recipeio/ui/UI.java
@@ -52,33 +52,25 @@ public class UI {
      * Prints list of accepted instructions.
      */
     public static void printInstructions(){
-        System.out.println("Accepted commands are:");
-        System.out.println("help: shows available commands\n");
-        System.out.println("list: shows you list of recipes");
-        System.out.println("\tInput Example: list\n");
-        System.out.println("delete LIST_NUMBER: deletes a recipe at a given index");
-        System.out.println("\tInput Example: delete 1\n");
-        System.out.println("find kw KEYWORD: finds recipes with a given keyword");
-        System.out.println("\tInput Example: find kw pizza\n");
-        System.out.println("find date YYYY-MM-DD: finds recipes added on a given date");
-        System.out.println("\tInput Example: find date 2024-03-28\n");
-        System.out.println("add NAME/MINUTES/KCALS/ALLERGIES/CATEGORY/URL: adds a recipe");
-        System.out.println("\tInput Example: add pizza/34/340/eggs/dinner/www.food.com\n");
-        System.out.println("exit: to leave the program");
+        System.out.println("\tAccepted commands are:");
+        System.out.println("\thelp: shows available commands\n");
+        System.out.println("\tlist: shows you list of recipes");
+        System.out.println("\t\tInput Example: list\n");
+        System.out.println("\tadd NAME/MINUTES/KCALS/ALLERGIES/CATEGORY/URL: adds a recipe");
+        System.out.println("\t\tInput Example: add pizza/34/340/eggs/dinner/www.food.com\n");
+        System.out.println("\tdelete LIST_NUMBER: deletes a recipe at a given index");
+        System.out.println("\t\tInput Example: delete 1\n");
+        System.out.println("\tfind kw KEYWORD: finds recipes with a given keyword");
+        System.out.println("\t\tInput Example: find kw pizza\n");
+        System.out.println("\tfind date YYYY-MM-DD: finds recipes added on a given date");
+        System.out.println("\t\tInput Example: find date 2024-03-28\n");
+        System.out.println("\tfilter ALLERGY: lists the recipes that do not contain this allergen");
+        System.out.println("\t\tInput Example: filter dairy\n");
+        System.out.println("\texit: to leave the program");
     }
 
-    public static void printMatches(ArrayList<Recipe> matches) {
-        int counter = 0;
-        StringBuilder output = new StringBuilder();
-        while (counter < matches.size()) {
-            Recipe selected = matches.get(counter);
-            output.append(selected.toString());
-            if (counter != matches.size() - 1) {
-                output.append("\n");
-            }
-            counter += 1;
-        }
-        System.out.println("Here are your matches:\n" + output);
+    public static void printRecipes(ArrayList<Recipe> matches) {
+        matches.forEach(recipe -> System.out.println("\t\t" + recipe));
     }
 
     /**
