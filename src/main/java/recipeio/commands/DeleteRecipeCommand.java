77005e3edfb65926cc8439213a30f4a3113fe913
yuki-zmstr <yukihide99@gmail.com>
2024-03-31 16:52:20 +0800

    Add JavaDoc to DeleteRecipeCommand

--- src/main/java/recipeio/commands/DeleteRecipeCommand.java
+++ src/main/java/recipeio/commands/DeleteRecipeCommand.java
@@ -9,8 +9,10 @@ public class DeleteRecipeCommand {
 
     /**
      * Deletes the Recipe from the recipe list.
+     * recipeNumber is subtracted by 1 due to 0 based indexing.
      *
      * @param recipeNumber The recipe number from the user.
+     * @param recipes The list of current recipes.
      */
     public static void execute(int recipeNumber, ArrayList<Recipe> recipes) {
         Recipe selectedRecipe = recipes.get(recipeNumber - 1);
