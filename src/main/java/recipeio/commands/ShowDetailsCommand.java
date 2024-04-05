43747d1afb4eda3547a59c78092225e391503c73
nidhi-nayak <nidhirnayak@gmail.com>
2024-04-05 13:02:33 +0800

    Fix allergies display in details command

--- src/main/java/recipeio/commands/ShowDetailsCommand.java
+++ src/main/java/recipeio/commands/ShowDetailsCommand.java
@@ -10,13 +10,14 @@ public class ShowDetailsCommand {
      * @param recipe The recipe to show details of.
      */
     public static void execute(Recipe recipe) {
-
         System.out.println("Here are the details:");
         System.out.println("\tName: " + recipe.name);
         System.out.println("\tCook time: " + recipe.cookTime + " min");
         System.out.println("\tCalories: " + recipe.calories + " kcal");
         System.out.print("\tAllergies: ");
-        recipe.allergies.forEach(System.out::print);
+        String allergiesWithBrackets = recipe.allergies.toString();
+        String allergiesNoBrackets = allergiesWithBrackets.substring(1, allergiesWithBrackets.length() - 1);
+        System.out.print(allergiesNoBrackets);
         System.out.println("\n\tCategory: " + recipe.category);
         System.out.println("\tDateAdded: " + recipe.dateAdded.toString());
         System.out.println("\tURL: " + recipe.url);
