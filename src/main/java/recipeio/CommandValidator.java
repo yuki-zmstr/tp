93a789495b07cd8b2dbdc55aacb1861a56d7f16e
HUNGPHAN <hungpd1975@gmail.com>
2024-04-10 00:19:18 +0800

    Fixes #146

--- src/main/java/recipeio/CommandValidator.java
+++ src/main/java/recipeio/CommandValidator.java
@@ -1,6 +1,7 @@
 package recipeio;
 
 import recipeio.constants.InputParserConstants;
+import recipeio.constants.StorageConstants;
 import recipeio.recipe.Recipe;
 import recipeio.constants.CommandValidatorConstants;
 
@@ -26,7 +27,7 @@ import static recipeio.constants.InputParserConstants.MEAL_CATEGORY_INDEX;
  * Class containing methods that validate a user's input into the command line.
  */
 public class CommandValidator {
-
+    public static final int ADD_COMMAND_ENRIES_START_INDEX = 3;
     /**
      * Splits a recipe name into individual words.
      *
@@ -200,12 +201,28 @@ public class CommandValidator {
         return true;
     }
 
+    /**
+     * Check if the entries of the add command contains the write delimiter.
+     * @param userInput The user's input from the CLI.
+     * @return false if the entries in the add command contain the write delimiter
+     * and true otherwise.
+     */
+    public static boolean isValidAddEntries(String userInput) {
+        String entries = userInput.substring(ADD_COMMAND_ENRIES_START_INDEX);
+        return !entries.contains(StorageConstants.WRITE_DELIMITER);
+    }
+
     public static boolean isValidAddCommand(String userInput) {
         String[] details = InputParser.splitUpAddInput(userInput);
         if (details.length != InputParserConstants.TOTAL_INGREDIENTS_INDEX) {
             System.out.println("The add function accepts 6 parameters: {name} {cook time} {calories}\n\t {singular " +
                     "space separated allergies} {meal category} {url}");
             System.out.println("Input Example: add pizza, 34, 340, egg dairy, dinner, www.food.com");
+            return false;
+        }
+
+        if (!isValidAddEntries(userInput)) {
+            System.out.println("Please remove the '|' character from the data entries.");
             return false;
         }
         String[] remainingInput = splitUpAddInput(userInput);
