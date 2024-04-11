7ec2c0cb872a63a4870cc236e3be9f6f7fe0a644
HUNGPHAN <hungpd1975@gmail.com>
2024-04-12 05:53:55 +0800

    modify test cases to not mess up assertEquals

--- src/test/java/commands/SortCommandsTest.java
+++ src/test/java/commands/SortCommandsTest.java
@@ -19,18 +19,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SortCommandsTest {
 
     ArrayList<Recipe> recipes = new ArrayList<>();
-
+    Recipe testRecipe1;
+    Recipe testRecipe2;
+    Recipe testRecipe3;
+    Recipe testRecipe4;
+    Recipe testRecipe5;
     void initRecipes() {
         ArrayList<String> allergies = new ArrayList<>();
-        Recipe testRecipe1 = new Recipe("pizza", 34, 340, allergies,
+        testRecipe1 = new Recipe("pizza", 34, 340, allergies,
                 MealCategory.DINNER, LocalDate.parse("2024-04-10"), "www.url.com");
-        Recipe testRecipe2 = new Recipe("pho", 100, 600, allergies,
+        testRecipe2 = new Recipe("pho", 100, 600, allergies,
                 MealCategory.BREAKFAST, LocalDate.parse("2024-03-12"), "www.pho.com");
-        Recipe testRecipe3 = new Recipe("banh mi", 5, 500, allergies,
+        testRecipe3 = new Recipe("banh mi", 5, 500, allergies,
                 MealCategory.BREAKFAST, LocalDate.parse("2022-01-12"), "www.banhmi.com");
-        Recipe testRecipe4 = new Recipe("bun bo hue", 480, 600, allergies,
+        testRecipe4 = new Recipe("bun bo hue", 480, 600, allergies,
                 MealCategory.LUNCH, LocalDate.parse("2013-04-12"), "www.bunbohue.com");
-        Recipe testRecipe5 = new Recipe("tra da", 2, 10, allergies,
+        testRecipe5 = new Recipe("tra da", 2, 10, allergies,
                 MealCategory.DESSERT, LocalDate.parse("2024-01-12"), "www.trada.com");
 
         recipes.add(testRecipe1);
@@ -43,105 +47,53 @@ public class SortCommandsTest {
     @Test
     public void testSortNamesAscending() {
         initRecipes();
-
-        String expected = "Here is a summary of your recipe book." + System.lineSeparator() +
-                "\t1. banh mi / added on 2022-01-12 / url: www.banhmi.com" + System.lineSeparator() +
-                "\t2. bun bo hue / added on 2013-04-12 / url: www.bunbohue.com" + System.lineSeparator() +
-                "\t3. pho / added on 2024-03-12 / url: www.pho.com" + System.lineSeparator() +
-                "\t4. pizza / added on 2024-04-10 / url: www.url.com" + System.lineSeparator() +
-                "\t5. tra da / added on 2024-01-12 / url: www.trada.com" + System.lineSeparator() +
-                System.lineSeparator() +
-                "To find out more about a particular recipe, try the 'detail {recipe number}' command."
-                + System.lineSeparator();
-
-        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(testOut);
-        System.setOut(printStream);
-
-        ArrayList<Recipe> result = SortAscendingNames.execute(recipes);
-        ListRecipeCommand.execute(result);
-
-        String actual = testOut.toString();
-
-        assertEquals(actual, expected);
+        ArrayList<Recipe> actual = SortAscendingNames.execute(recipes);
+        ArrayList<Recipe> expected = new ArrayList<>();
+        expected.add(testRecipe3);
+        expected.add(testRecipe4);
+        expected.add(testRecipe2);
+        expected.add(testRecipe1);
+        expected.add(testRecipe5);
+        assertEquals(actual.toString(), expected.toString());
     }
 
     @Test
     public void testSortAscendingDateAdded() {
         initRecipes();
-
-        String expected = "Here is a summary of your recipe book." + System.lineSeparator() +
-                "\t1. bun bo hue / added on 2013-04-12 / url: www.bunbohue.com" + System.lineSeparator() +
-                "\t2. banh mi / added on 2022-01-12 / url: www.banhmi.com" + System.lineSeparator() +
-                "\t3. tra da / added on 2024-01-12 / url: www.trada.com" + System.lineSeparator() +
-                "\t4. pho / added on 2024-03-12 / url: www.pho.com" + System.lineSeparator() +
-                "\t5. pizza / added on 2024-04-10 / url: www.url.com" + System.lineSeparator() +
-                System.lineSeparator() +
-                "To find out more about a particular recipe, try the 'detail {recipe number}' command."
-                + System.lineSeparator();
-
-        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(testOut);
-        System.setOut(printStream);
-
-        ArrayList<Recipe> result = SortAscendingDateAdded.execute(recipes);
-        ListRecipeCommand.execute(result);
-
-        String actual = testOut.toString();
-
-        assertEquals(actual, expected);
+        ArrayList<Recipe> actual = SortAscendingDateAdded.execute(recipes);
+        ArrayList<Recipe> expected = new ArrayList<>();
+        expected.add(testRecipe4);
+        expected.add(testRecipe3);
+        expected.add(testRecipe5);
+        expected.add(testRecipe2);
+        expected.add(testRecipe1);
+        assertEquals(actual.toString(), expected.toString());
 
     }
 
     @Test
     public void testSortAscendingCalories() {
         initRecipes();
-
-        String expected = "Here is a summary of your recipe book." + System.lineSeparator() +
-                "\t1. tra da / added on 2024-01-12 / url: www.trada.com" + System.lineSeparator() +
-                "\t2. pizza / added on 2024-04-10 / url: www.url.com" + System.lineSeparator() +
-                "\t3. banh mi / added on 2022-01-12 / url: www.banhmi.com" + System.lineSeparator() +
-                "\t4. pho / added on 2024-03-12 / url: www.pho.com" + System.lineSeparator() +
-                "\t5. bun bo hue / added on 2013-04-12 / url: www.bunbohue.com" + System.lineSeparator() +
-                System.lineSeparator() +
-                "To find out more about a particular recipe, try the 'detail {recipe number}' command."
-                + System.lineSeparator();
-
-        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(testOut);
-        System.setOut(printStream);
-
-        ArrayList<Recipe> result = SortAscendingCalories.execute(recipes);
-        ListRecipeCommand.execute(result);
-
-        String actual = testOut.toString();
-
-        assertEquals(actual, expected);
+        ArrayList<Recipe> actual = SortAscendingCalories.execute(recipes);
+        ArrayList<Recipe> expected = new ArrayList<>();
+        expected.add(testRecipe5);
+        expected.add(testRecipe1);
+        expected.add(testRecipe3);
+        expected.add(testRecipe2);
+        expected.add(testRecipe4);
+        assertEquals(actual.toString(), expected.toString());
     }
 
     @Test
     public void testSortAscendingCookTime() {
         initRecipes();
-
-        String expected = "Here is a summary of your recipe book." + System.lineSeparator() +
-                "\t1. tra da / added on 2024-01-12 / url: www.trada.com" + System.lineSeparator() +
-                "\t2. banh mi / added on 2022-01-12 / url: www.banhmi.com" + System.lineSeparator() +
-                "\t3. pizza / added on 2024-04-10 / url: www.url.com" + System.lineSeparator() +
-                "\t4. pho / added on 2024-03-12 / url: www.pho.com" + System.lineSeparator() +
-                "\t5. bun bo hue / added on 2013-04-12 / url: www.bunbohue.com" + System.lineSeparator() +
-                System.lineSeparator() +
-                "To find out more about a particular recipe, try the 'detail {recipe number}' command."
-                + System.lineSeparator();
-
-        ByteArrayOutputStream testOut = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(testOut);
-        System.setOut(printStream);
-
-        ArrayList<Recipe> result = SortAscendingCookTime.execute(recipes);
-        ListRecipeCommand.execute(result);
-
-        String actual = testOut.toString();
-
-        assertEquals(actual, expected);
+        ArrayList<Recipe> actual = SortAscendingCookTime.execute(recipes);
+        ArrayList<Recipe> expected = new ArrayList<>();
+        expected.add(testRecipe5);
+        expected.add(testRecipe3);
+        expected.add(testRecipe1);
+        expected.add(testRecipe2);
+        expected.add(testRecipe4);
+        assertEquals(actual.toString(), expected.toString());
     }
 }
